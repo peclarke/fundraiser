80dcd36ba1dedecc3691a7e6bb69d10ff162a79e
Paul Clarke <paulioclarke@icloud.com>
2024-04-26 17:51:56 +1000

    shimmying files around to start a server

---
+++ frontend/src/state.ts
@@ -0,0 +1,77 @@
+import { Action, action, createStore } from "easy-peasy";
+
+export type Milestone = {
+    goal: number;
+    desc: string;
+}
+
+export type StoreModel = {
+    // state
+    donations: number;
+    milestones: Milestone[];
+
+    // actions
+    addDonation: Action<StoreModel, number>;
+    setDonation: Action<StoreModel, number>;
+    setMilestones: Action<StoreModel, Milestone[]>;
+}
+
+const milestones = [
+    {
+        goal: 20,
+        desc: "Something something"
+    },
+    {
+        goal: 30,
+        desc: "WHAT"
+    },
+    {
+        goal: 50,
+        desc: "burn the garden"
+    },
+    {
+        goal: 50,
+        desc: "burn the garden"
+    },
+    {
+        goal: 50,
+        desc: "burn the garden"
+    },
+    {
+        goal: 50,
+        desc: "burn the garden"
+    },
+    {
+        goal: 50,
+        desc: "burn the garden"
+    }
+]
+
+const model: StoreModel = {
+    donations: 0,
+    milestones: milestones,
+    addDonation: action((state, payload) => {
+        state.donations += payload;
+    }),
+    setDonation: action((state, payload) => {
+        state.donations = payload;
+    }),
+
+    setMilestones: action((state, payload) => {
+        state.milestones = payload;
+    })
+}
+
+const store = createStore<StoreModel>(model)
+
+// Set up hot reloading
+// https://easy-peasy.vercel.app/docs/recipes/hot-reloading.html
+if (import.meta.env.NODE_ENV === "development") {
+    if (module.hot) {
+      module.hot.accept("./model", () => {
+        store.reconfigure(model);
+      });
+    }
+}
+
+export default store;
